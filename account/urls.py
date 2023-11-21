9e7d9d04a64f7315714e3e34a0f5a66cbb0caea9
intiqamquluzada <quluzadintiqam@gmail.com>
2023-11-21 21:59:08 +0400

    forgot password

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from account.views import *
 
-
 urlpatterns = [
 
     path("register/", register_view, name='register'),
     path("otp/<slug>/", otp_view, name='otp'),
+    path("email/", email_view, name='email'),
+    path("forgot/check/<slug>/", forgot_checker, name='forgot_otp'),
+    path("reset/password/<slug>/", reset_password, name='reset_password'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -5,6 +5,9 @@ from services.generator import Generator
 from account.models import MyUser as User
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import get_object_or_404
+
+
 
 def register_view(request):
     form = RegistrationUserForm()
@@ -30,12 +33,6 @@ def register_view(request):
 
             return redirect("otp", slug=new_user.slug)
 
-
-
-
-
-
-
     context = {
         "form": form,
     }
@@ -55,3 +52,54 @@ def otp_view(request, slug):
 
     context = {}
     return render(request, "otp.html", context)
+
+
+def email_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        users = User.objects.filter(email=email)
+        if users.exists():
+            user = get_object_or_404(User, email=email)
+            password_reset_code = Generator.create_activation_code(size=8, model_=User)
+            user.password_reset_code = password_reset_code
+            user.save()
+
+            subject = "Activation Message"
+            message = f"CODE: {password_reset_code}"
+            from_mail = settings.EMAIL_HOST_USER
+            to_mail = [user.email]
+
+            send_mail(
+                subject, message, from_mail, to_mail
+            )
+            return redirect("forgot_otp", slug=user.slug)
+
+    context = {}
+    return render(request, "email.html", context)
+
+
+def forgot_checker(request, slug):
+    user = get_object_or_404(User, slug=slug)
+    if request.method == "POST":
+        forgot_otp = request.POST.get("forgototp")
+        if user.password_reset_code == forgot_otp:
+            return redirect("reset_password", slug=user.slug)
+        else:
+            return redirect("/")
+
+    context = {}
+    return render(request, "forgot-checker.html", context)
+
+
+def reset_password(request, slug):
+    user = get_object_or_404(User, slug=slug)
+    if request.method == "POST":
+        pwd1 = request.POST.get("pwd1")
+        pwd2 = request.POST.get("pwd2")
+
+        if pwd1 == pwd2:
+            user.set_password(pwd1)
+            login(request, user)
+            return redirect("service")
+    context = {}
+    return render(request, "reset_password.html", context)
